2737c4e739e336ca723022b5e22c132880fceffa
alisonands <131694547+alisonands@users.noreply.github.com>
2023-11-09 12:06:06 -0500

    edits

--- pybank/main.py
+++ pybank/main.py
@@ -1,6 +1,8 @@
+#%%
 #imports
 import os
 import csv
+import numpy as np
 
 csvpath = 'pybank/Resources/budget_data.csv'
 
@@ -23,17 +25,35 @@ if sum(pnls)>=0:
 else:
     print(f"The net total losses are {sum(pnls)}")
 
-#%%
 #changes in profits/losses
 
 #loop through all profits and losses and find the difference
 #append differences to list
 #average the list
+
 differences = []
+max = 0
+min = 0
 
 for i in range(len(pnls)):
-    difference = pnls[i] - pnls[i+1]
+    difference = pnls[i+1] - pnls[i]
     differences.append(difference)
-    if i == (len(pnls)-1):
+    if difference > max:
+        max = difference
+        max_i = i+1
+    if difference < min:
+        min = difference
+        min_i = i+1
+    if i == (len(pnls)-2):
         break
-print(differences)
+print(f'The average of all these differences is {np.mean(differences)} \n')
+
+print(f"The greatest increase occurred at {max_i} when the value increased from {pnls[max_i-1]} to {pnls[max_i]} \n where the difference was {max}")
+print(f"This is an increase of {(max/pnls[max_i+1])*100}%!")
+print(f"This occurred during the month {dates[max_i]} \n")
+
+print(f"The greatest decrease occurred at {min_i} when the value decreased from {pnls[min_i-1]} to {pnls[min_i]} \n where the difference was {min}")
+print(f"This is an decrease of {(min/pnls[min_i+1])*100}%!")
+print (f"This occurred during the month {dates[min_i]}")
+
+# %%
